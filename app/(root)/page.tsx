5187e1928f89ebd440563b2a143e72e97df1aa96
benjamin <benjaminrenny@gmail.com>
2025-11-20 14:26:08 -0800

    Implementing tradingview hooks and widgets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,31 @@
-import { Button } from "@/components/ui/button"
+import TradingViewWidget from "@/components/TradingViewWidget"
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants"
 
 
-const Home: ()=>Element = () => {
+const Home = () => {
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
   return (
-    <div className="flex home-wrapper cursor-pointer min-h-screen">
-      Home Page
+    <div className="flex home-wrapper  min-h-screen">
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget title='Market overview' scriptUrl={`${scriptUrl}market-overview.js`} config={MARKET_OVERVIEW_WIDGET_CONFIG} height={600} className="custom-chart" />
+        </div>
+
+        <div className="md:col-span-1 xl:col-span-2">
+          <TradingViewWidget title='Stock Heatmap' scriptUrl={`${scriptUrl}stock-heatmap.js`} config={HEATMAP_WIDGET_CONFIG} height={600} className="custom-chart" />
+        </div>
+      </section>
+
+      <section className="grid w-full gap-8 home-section">
+        <div className="h-full md:col-span-1 xl:col-span-1">
+          <TradingViewWidget scriptUrl={`${scriptUrl}timeline.js`} config={TOP_STORIES_WIDGET_CONFIG} height={600} />
+        </div>
+
+        <div className="h-full md:col-span-1 xl:col-span-2">
+          <TradingViewWidget scriptUrl={`${scriptUrl}market-quotes.js`} config={MARKET_DATA_WIDGET_CONFIG} height={600} />
+        </div>
+      </section>
     </div>
   )
 }
